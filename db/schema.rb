fce0c3c11df3ad33352f45d002c43aaf4b0feac5
amykangweb <selene6023@gmail.com>
2016-05-26 15:31:58 -0700

    Generate post scaffold.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526220651) do
+ActiveRecord::Schema.define(version: 20160526223110) do
 
   create_table "meows", force: :cascade do |t|
     t.string   "word"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
