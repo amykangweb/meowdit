f9b02b4beeee6d6f65e42d26d226fd7f2638fc96
amykangweb <selene6023@gmail.com>
2016-07-19 13:34:28 -0700

    Split replace with meow method with separate methods for title and content. Added before update callback to replace post conent with meows.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,19 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, presence: true
 
-  before_save :replace_with_meow
+  before_save :replace_title_with_meow, :replace_content_with_meow
+  before_update :replace_title_with_meow, :if => :title_changed?
+  before_update :replace_content_with_meow, :if => :content_changed?
 
   private
 
-  def replace_with_meow
+  def replace_title_with_meow
     meows1 = Meow.offset(rand(Meow.count)).limit(self.title.split.count)
-    meows2 = Meow.offset(rand(Meow.count)).limit(self.content.split.count)
     self.title = meows1.map(&:word).join(" ")
+  end
+
+  def replace_content_with_meow
+    meows2 = Meow.offset(rand(Meow.count)).limit(self.content.split.count)
     self.content = meows2.map(&:word).join(" ")
   end
 end
